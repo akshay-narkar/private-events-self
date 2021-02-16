cb08911019913135ea1877b4ab02e89960fef0e8
akshay-narkar <akshaynarkar1295@gmail.com>
2021-02-16 19:50:27 +0530

    Add event date column, update views, add upcoming and past events, update controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_151054) do
+ActiveRecord::Schema.define(version: 2021_02_16_105349) do
 
   create_table "events", force: :cascade do |t|
     t.text "description"
     t.integer "creator_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "eventdate"
   end
 
   create_table "users", force: :cascade do |t|
